764179d010b8c7e24a8de12dabb48927793bf3f2
Nicolae Critu <nicolae.critu@atos.net>
2019-03-04 19:44:52 +0200

    functions *args and **kwargs

--- students.py
+++ students.py
@@ -8,3 +8,15 @@ add_student("John", 3)
 add_student("Jane")
 
 print(list(students))
+
+
+def var_args(name, *args):
+    print(name)
+    print(args)
+
+
+def kwargs(name, **kwargs):
+    print(name)
+    print(kwargs["description"], kwargs["feedback"])
+
+var_args("Mark", "Loves python", None, True, "Hello")
