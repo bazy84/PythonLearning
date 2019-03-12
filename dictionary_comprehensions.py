3b0a33054eb6ec90d439c3d68796cfad2a13d174
Nicolae Critu <nicolae.critu@atos.net>
2019-03-12 18:01:19 +0200

    playing with dictionaries and for arg1, arg2 in dictionary.items() fun

---
+++ dictionary_comprehensions.py
@@ -0,0 +1,16 @@
+#import pdb
+from pprint import pprint as pp
+
+country_to_capital = {'United Kingdom': 'London',
+                      'Brazil': 'Brazilia',
+                      'Morocco': 'Rabat',
+                      'Sweden': 'Stockholm'}
+#pdb.set_trace()
+capital_to_country = {capital: country for country, capital in country_to_capital.items()}
+
+def main():
+    pp(country_to_capital)
+    pp(capital_to_country)
+
+if __name__ == "__main__":
+    main()
