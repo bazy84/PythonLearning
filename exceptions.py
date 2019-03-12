bd7d0af41e3b44abec22b2a1512d966fbd5423f4
Nicolae Critu <nicolae.critu@atos.net>
2019-03-12 15:04:46 +0200

    more fancy try: except: finally

--- exceptions.py
+++ exceptions.py
@@ -1,11 +1,27 @@
+import sys
+
+
 def convert(s):
     '''Convert to an integer'''
     try:
-        x = int(s)
-        print("Conversion succeeded! x =", x)
-    except (ValueError, TypeError):
-        print("Conversion failed!")
-        x = -1
-    return x
+        return int(s)
+    except (ValueError, TypeError) as e:
+        print("Conversion error: {}" \
+            .format(str(e)),
+            file=sys.stderr)
+    return -1
 
 # IndentationError, SyntaxError and NameError: You should NOT normally catch this!
+
+import os
+
+def make_at(path, dir_name):
+    original_path = os.getcwd()
+    try:
+        os.chdir(path)
+        os.mkdir(dir_name)
+    except OSError as e:
+        print(e, file=sys.stderr)
+        raise
+    finally: #finally-block is executed no matter how the try-block exits
+        os.chdir(original_path)
